3bb3139a6eed97d3d6f078aa2079f71e3fd140da
Vito-28 <vito.chianese@outlook.it>
2026-03-22 23:21:53 +0100

    feat: add demo in main class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import model.Configuration;
+import model.ConfigurationVersion;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Configuration configuration = new Configuration("database-config");
+
+        configuration.addVersion(new ConfigurationVersion(1, "db.host=localhost"));
+        configuration.addVersion(new ConfigurationVersion(2, "db.host=db.company.com"));
+        configuration.addVersion(new ConfigurationVersion(3, "db.host=cloud-db"));
+
+        for(ConfigurationVersion version : configuration.getVersions()) {
+            System.out.println("Version: " + version.getVersionNumber());
+            System.out.println(version.getContent());
+        }
     }
 }
