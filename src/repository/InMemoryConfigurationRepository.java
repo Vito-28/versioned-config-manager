75fe17f5b9b5c472bdd5544f5786df9c260b828e
Vito-28 <vito.chianese@outlook.it>
2026-04-12 22:14:49 +0200

    feat: add InMemoryConfigurationRepository implement interface repository

---
+++ src/repository/InMemoryConfigurationRepository.java
@@ -0,0 +1,20 @@
+package repository;
+
+import model.Configuration;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class InMemoryConfigurationRepository implements ConfigurationRepository{
+    private Map<String, Configuration> storage = new HashMap<>();
+
+    @Override
+    public void save(Configuration config) {
+        storage.put(config.getName(), config);
+    }
+
+    @Override
+    public Configuration findByName(String name) {
+        return storage.get(name);
+    }
+}
