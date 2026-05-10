b171e9897be691284c0b7a9f2e848a9458f3b14e
Vito-28 <vito.chianese@outlook.it>
2026-05-10 18:22:56 +0200

    extend ConfigurationRepository with findAllConfiguration

--- src/main/java/com/vitochianese/configmanager/repository/ConfigurationRepository.java
+++ src/main/java/com/vitochianese/configmanager/repository/ConfigurationRepository.java
@@ -3,7 +3,10 @@ package com.vitochianese.configmanager.repository;
 
 import com.vitochianese.configmanager.model.Configuration;
 
+import java.util.List;
+
 public interface ConfigurationRepository {
     public void save(Configuration config);
     public Configuration findByName(String name);
+    public List<Configuration> findAllConfiguration();
 }
