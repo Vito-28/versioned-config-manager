aaa9a5bb5aacba53e815723ce765242d86850e1d
Vito-28 <vito.chianese@outlook.it>
2026-05-11 21:16:48 +0200

    feat: add JSON persistence support to ConfigurationService

--- src/main/java/com/vitochianese/configmanager/service/ConfigurationService.java
+++ src/main/java/com/vitochianese/configmanager/service/ConfigurationService.java
@@ -5,18 +5,42 @@ import com.vitochianese.configmanager.model.Configuration;
 import com.vitochianese.configmanager.model.ConfigurationVersion;
 import com.vitochianese.configmanager.repository.ConfigurationRepository;
 import com.vitochianese.configmanager.repository.InMemoryConfigurationRepository;
+import com.vitochianese.configmanager.util.JsonUtil;
 
 
+import java.io.IOException;
 import java.util.List;
 
 public class ConfigurationService {
-    private final ConfigurationRepository repository = new InMemoryConfigurationRepository();
+    private final ConfigurationRepository repository;
+    private final JsonUtil jsonUtil;
+
+    public ConfigurationService() throws IOException {
+        this.repository = new InMemoryConfigurationRepository();
+        this.jsonUtil = new JsonUtil();
+    }
 
     public ConfigurationRepository getRepository() {
         return repository;
     }
 
-    public void createConfiguration(String name) {
+    public Configuration getConfigurationByName(String name) {
+        return repository.findByName(name);
+    }
+
+    public List<Configuration> getAllConfigurations() {
+        return repository.findAllConfigurations();
+    }
+
+    public void save() throws IOException {
+        jsonUtil.saveToFile(repository.findAllConfigurations());
+    }
+
+    public List<Configuration> load() throws IOException {
+        return jsonUtil.loadFromFile();
+    }
+
+    public void createConfiguration(String name) throws IOException {
         repository.save(new Configuration(name));
     }
 
@@ -34,7 +58,7 @@ public class ConfigurationService {
         return repository.findByName(configuration.getName()).getVersions();
     }
 
-    public void rollbackVersion(ConfigurationVersion version, Configuration configuration) throws ConfigurationNotFoundException {
+    public void rollbackVersion(ConfigurationVersion version, Configuration configuration) throws ConfigurationNotFoundException, IOException {
         if(configuration == null) {
             throw new ConfigurationNotFoundException("Configuration not found!");
         }
